b2bfd05ec3fb3490e9a7a9e344ec88c3d877e6e0
Alejandro Aguero Diaz <hykette@gmail.com>
2024-03-26 10:09:40 +0900

    Variables con mejor nombre y uso de len()

--- extras/funciones.py
+++ extras/funciones.py
@@ -1,13 +1,11 @@
 
 def promedio(pelis):
     """ Ingresa una lista donde el segundo indice sea tipo numero y calcula el promedio de la lista """
-    x = 0
-    i = 0
+    promedio = 0
     for peli in pelis:
-        x += peli[1] #En el indice 1 estaria la duracion
-        i += 1
-    x /= i
-    return x
+        promedio += peli[1] #En el indice 1 estaria la duracion
+    promedio /= len(pelis)
+    return promedio
 
 def mayor_al_promedio(pelis, promedio):
     """ Recibe una lista donde el segundo indice sea tupo numero y un promedio, con eso, calcula cuantas peliculas son mayores al promedi """
